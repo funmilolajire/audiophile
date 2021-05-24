5fe1ebef53e74f726ca5f4a0d86d043950d3e451
Funmilola O <funmilolajire@gmail.com>
2021-05-24 10:02:09 +0100

    nonserialized object error

--- client/pages/categories/[category].tsx
+++ client/pages/categories/[category].tsx
@@ -16,7 +16,12 @@ const category: FC<CategoryProducts> = ({ categoryProducts }) => {
         description: "Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week."
     }
     useEffect(() => {
-        setProducts(categoryProducts)
+        setProducts((prev: CategoryProduct[]) => {
+            return categoryProducts.map(product => ({
+                ...product,
+                categoryThumbnails: JSON.parse(JSON.stringify(product.categoryThumbnails))
+            }))
+        })
     }, [categoryProducts])
     return (
         <>
